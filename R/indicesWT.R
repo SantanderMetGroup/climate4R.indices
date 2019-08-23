c07530de018a17753a3fb551b0721fc5fcc9c52d
jbedia <joaquin.bedia@gmail.com>
2019-08-23 14:12:03 +0200

    Fix error in example

--- R/indicesWT.R
+++ R/indicesWT.R
@@ -19,46 +19,38 @@
 #' @description Calculate circulation indices of grids or multimember grids. 
 #' @param grid A grid (gridded or station dataset), or multimember grid object of geopotential height or geopotential.
 #' @param season Selected month(s) for the calculation. Default: NULL (i.e. as input grid).
-#' @param cluster.type Clustering type that matches with the WT circulation index to be computed. 
-#' @param centers Integer value indicating the number of clusters, \strong{k}, or center points. See \link[TransformeR]{clusterGrid} for more details.
+#' @param cluster.type Weather typing method. See details.
+#' @param centers Integer value indicating the number of clusters, \strong{k}, or center points. See details.
+#' @details The clustering parameters for weather typing are internally passed to \code{\link[transformeR]{clusterGrid}}. 
 #' @return The WT circulation indices (and members, if applicable) with:
 #' \itemize{
-#' * index: vector with the corresponding cluster from each point of the series.
-#' * pattern: Array with the spatial pattern of the clusters obtained.
-#' * dates and coordinates.
-#' * further arguments related to the WT indices, such as the corresponding cluster type, number of clusters, etc.
+#' \item index: vector with the corresponding cluster from each point of the series.
+#' \item pattern: Array with the spatial pattern of the clusters obtained.
+#' \item dates and coordinates.
+#' \item further arguments related to the WT indices, such as the corresponding cluster type, number of clusters, etc.
 #' }
-#'  
 #' @export
-#' 
-#' @examples \dontrun{ 
+#' @examples 
 #' data(NCEP_hgt500_2001_2010)
-#' wt <- indicesWT(grid=NCEP_hgt500_2001_2010, season=1, season=season, cluster.type="kmeans", centers=10)
-#' }
+#' wt <- indicesWT(grid=NCEP_hgt500_2001_2010, season=1,
+#'                 cluster.type="kmeans", centers = 3)
 
 
-indicesWT <- function(grid, season, cluster.type, centers=centers 
+
+indicesWT <- function(grid, season, cluster.type, centers = NULL
                       #rot=rot, members=members
-                      ){
-  
-  cluster.type <- match.arg(cluster.type, choices = c("kmeans", "som", "hierarchical","lamb"), several.ok = FALSE)
-  
+                      ) {
+  cluster.type <- match.arg(cluster.type, choices = c("kmeans", "som", "hierarchical", "lamb"))
   # *** SUBSET WITH MONTHS FROM ARGUMENT "SEASON" ***
   data.mon <- subsetGrid(grid = grid, season = season)
-  
-  
   #  *** CALCULATE CLUSTERS BY MONTH*** 
   # depending on the index.code call to a different clustering type.
   # argument k needs to be provided to circIndexGrid()
- 
   #wt <- transformeR::clusterGrid(data.mon, type = cluster.type, centers=centers)
-  wt <- clusterGrid(data.mon, type = cluster.type, centers=centers)
+  wt <- clusterGrid(data.mon, type = cluster.type, centers = centers)
   wt$index <- attr(wt, "clusters")
   attr(wt, "clusters") <- NULL
   wt$pattern <- wt$Data 
   wt$Data <- NULL
-  
-  
   return(wt)
-  
 }
